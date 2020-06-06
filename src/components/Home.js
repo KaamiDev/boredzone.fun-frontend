d758b0c4e2315f08dbcd9b729017dafca8d94397
= <=>
2020-06-06 13:45:18 -0400

    Styled header in home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 	return (
-		<div>
-			<p>Home</p>
+		<div className="home-body">
+			<div className="header-section">
+				<h1>Welcome to BoredZone.fun!</h1>
+				<h3>
+					If you're bored during social distancing then you've come to the right place! BoredZone.fun is the
+					best ever-growing collection of user submitted ideas of things to do while stuck at home.
+				</h3>
+				<div className="header-buttons">
+					<Link to="/browse">Browse Ideas</Link>
+					<Link to="/login">Login</Link>
+				</div>
+			</div>
 		</div>
 	);
 };
