d62ab06a1c8f9da26bd1c348f9310af42ff15b91
= <=>
2020-06-06 13:27:15 -0400

    Styled Navbar Component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,11 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => {
 	return (
-		<div>
-			<Link to="/">Home</Link>
-			<Link to="/browse">Browse</Link>
-			<Link to="/login">Login</Link>
-			<Link to="/register">Register</Link>
+		<div className="navbar">
+			<div className="nav-heading">
+				<Link to="/">
+					<h2>BoredZone.fun</h2>
+				</Link>
+			</div>
+			<div className="nav-links">
+				<Link to="/" style={{ color: '#fff' }}>
+					Home
+				</Link>
+				<Link to="/browse">Browse Ideas</Link>
+				<Link to="/login">Login</Link>
+				<Link to="/register">Register</Link>
+			</div>
 		</div>
 	);
 };
