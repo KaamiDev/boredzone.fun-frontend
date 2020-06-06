2c0d56cdb57b60b42b5f84a6263f4fc00077a259
= <=>
2020-06-06 18:09:12 -0400

    created styling for login and register components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 	return (
-		<div>
-			<p>Login</p>
+		<div className="login-contents">
+			<div className="login-title">
+				<h2>
+					Login to an <span>Account</span>
+				</h2>
+				<h5>
+					You can login to an account below. Alternatively, you can{' '}
+					<Link className="color-link" to="/register">
+						register
+					</Link>.
+				</h5>
+			</div>
+			<div className="login-card shadow">
+				<h3>Login Below.</h3>
+				<input placeholder="Email" />
+				<input placeholder="Password" />
+				<button className="universal-btn login-btn">Login</button>
+			</div>
 		</div>
 	);
 };
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
 	return (
-		<div>
-			<p>Register</p>
+		<div className="login-contents">
+			<div className="login-title">
+				<h2>
+					Register for an <span>Account</span>
+				</h2>
+				<h5>
+					You can register for an account below. Alternatively, you can{' '}
+					<Link className="color-link" to="/login">
+						login
+					</Link>.
+				</h5>
+			</div>
+			<div className="login-card shadow">
+				<h3>Register Below.</h3>
+				<input placeholder="Username" />
+				<input placeholder="Email" />
+				<input placeholder="Password" />
+				<input placeholder="Confirm Password" />
+				<button className="universal-btn login-btn">Register</button>
+			</div>
 		</div>
 	);
 };
