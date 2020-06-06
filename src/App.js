ec8250d06413f632478e77689b4c527b7b579d68
= <=>
2020-06-06 13:02:38 -0400

    Setup basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Browse from './components/Browse';
+import Login from './components/Login';
+import Register from './components/Register';
+
 import './App.css';
 
 function App() {
 	return (
-		<div className="App">
-			<p>Hello World!</p>
-		</div>
+		<Router>
+			<div>
+				<Nav />
+				<Switch>
+					<Route path="/" exact component={Home} />
+					<Route path="/browse" component={Browse} />
+					<Route path="/login" component={Login} />
+					<Route path="/register" component={Register} />
+				</Switch>
+			</div>
+		</Router>
 	);
 }
 
